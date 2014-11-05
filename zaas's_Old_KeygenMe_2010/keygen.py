b7fa7d58626b239a66e328fb397ac6e4207e459d
phreak <baderj@gmx.net>
2014-11-05 15:26:11 +0100

    missing ciphertexts for DCTF crackme

--- zaas's_Old_KeygenMe_2010/keygen.py
+++ zaas's_Old_KeygenMe_2010/keygen.py
@@ -13,12 +13,14 @@ def keygen(name):
         avg = (code_sum - sum(code_list)) // (4-i)
         code_list[i] = min(nice_ascii_nr, key=lambda x: abs(x-avg))
     code_list[3] = code_sum - sum(code_list)
+    print(code_list)
     code = "".join([chr(c) for c in code_list] )
     return code
 
 parser = argparse.ArgumentParser("Keygen for Old_KeygenMe.exe")
 parser.add_argument("name")
 args = parser.parse_args()
+print(">{}<".format(args.name))
 if len(args.name) < 4:
     print("Name must have at least 4 characters")
     quit()
